b0c102d8401bac969c73ecab94c623a922cb4aa3
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-09-27 11:21:47 +0000

    added badge and navbar code

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Context } from '../store/appContext';
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context)
+    const { favorites } = store
+
 	return (
 		<nav className="navbar navbar-dark bg-dark mb-4 ps-1 py-4">
-			<span className="navbar-brand mb-0 ms-5 h1">StarWars Logo</span>
+			<span className="navbar-brand mb-0 ms-5 h1">
+				<img src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254" className="logo-img"></img>
+			</span>
 			
+			<div className="btn-group me-5">
+                <button className="btn btn-primary dropdown-toggle position-relative" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Favorites 
+					<span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>
+                        {favorites.length}
+                    </span>
+                </button>
+                <ul className="dropdown-menu">
+                    {favorites.length == 0 ? <p className='text-center'>... Empty</p> : favorites.map((item, index) => {
+                        return (
+                            <li className='d-flex justify-content-between align-items-center p-1'
+                                key={index}>
+                                {favorites[index].character.name}
+                                <a href="#" onClick={() => { actions.deleteFavorite(item, index); }}>X</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
 		</nav>
 	);
 };
